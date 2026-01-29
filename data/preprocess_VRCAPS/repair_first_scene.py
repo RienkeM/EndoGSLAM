61f530deeb0fc9d3c9c88a6d3626e71239441346
RienkeM <rienkemooiweer@gmail.com>
2026-01-29 10:05:33 +0100

    Rename files Unity import

--- data/preprocess_VRCAPS/repair_first_scene.py
+++ data/preprocess_VRCAPS/repair_first_scene.py
@@ -1,20 +1,45 @@
 import os
 import re
+import shutil
 
-def rename_files(folder_path):
+def rename_files(raw_folder_path, new_folder_path):
+    """
+    VR-CAPS export has a different name of color and depth picture
+    Also it starts counting from 0000 instead of 0001 so that should be changed
+    Copy the raw data instead of overwriting
+    """
 
-    files = os.listdir(folder_path)
+    # Create the destination folder if it doesn't exist
+    if os.path.exists(new_folder_path):
+        print(f"Clearing existing folder: {new_folder_path}")
+        shutil.rmtree(new_folder_path) # delete previous preprocessing attempts
+    os.makedirs(new_folder_path)
 
-    pattern = re.compile(r'^(\d+)_color\.png$')
+    files = os.listdir(raw_folder_path)
+
+    pattern = re.compile(r'^color_(\d+)\.png$')
+    pattern2 = re.compile(r'^depth_(\d+)\.exr$')
 
     for filename in files:
-        match = pattern.match(filename)
-        if match:
-            number = match.group(1)
-            new_number = number.zfill(4)
-            new_filename = f"{new_number}_color.png"
-            os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
-            print(f"Renamed {filename} to {new_filename}")
+        match_color = pattern.match(filename)
+        match_depth = pattern2.match(filename)
 
-folder_path = 'cecum_t1_a'
-rename_files(folder_path)
+        if match_color:
+            frame_number = int(match_color.group(1)) + 1
+            padded_number = str(frame_number).zfill(4)
+            new_filename = f"{padded_number}_color.png"
+        elif match_depth:
+            frame_number = int(match_depth.group(1)) + 1
+            padded_number = str(frame_number).zfill(4)
+            new_filename = f"{padded_number}_depth.exr"
+        else:
+            continue
+
+        src = os.path.join(raw_folder_path, filename)
+        dst = os.path.join(new_folder_path, new_filename)
+        shutil.copy2(src, dst) # copy content and move to new folder with new name
+        print(f"Processed: {filename} -> {new_filename}")
+        
+raw_folder_path = 'VR_CAPS_raw'
+new_folder_path = 'VR_CAPS'
+rename_files(raw_folder_path, new_folder_path)
